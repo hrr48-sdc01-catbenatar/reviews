171dc8256ab167939d839fd02918c19bafba7c63
Harsh Vikram Singh <singh.harshvikram@gmail.com>
2020-10-25 10:29:44 +0530

    added Cassandra server file

--- Server/cassandraServer.js
+++ Server/cassandraServer.js
@@ -3,24 +3,53 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cassandra = require('cassandra-driver');
+const port = process.env.WEBSERVERPORT;
 
 const app = express();
 
 app.use(logger('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
+app.use('/', express.static(__dirname + '/../Public'))
 app.use('/:id', express.static(__dirname + '/../Public'));
 
 const client = new cassandra.Client({
-  contactPoints: ['localhost'],
-  keyspace: 'testing_cassandra',
-  localDataCenter: 'datacenter1'
+  contactPoints: [process.env.CONTACTPOINTS],
+  keyspace: process.env.KEYSPACE,
+  localDataCenter: process.env.LOCALDATACENTER
 });
 
-const query = 'select * from tarjay_reviews where product_id=5 limit 2;';
+app.listen(port, () => {
+  console.log('///////////////////////////////////////////////////////////////////')
+  console.log(' Tarjay Reviews service running on port: ', port, 'with Cassandra');
+  console.log('///////////////////////////////////////////////////////////////////')
+})
 
-client.execute(query)
-  .then(result => {
-    console.log(result.rows);
+app.get('/api/reviews/:id', async (req, res) => {
+  let id = req.params.id;
+  if (id === undefined) {
+    id = 1;
+  }
+  console.log('id: ', id);
+  const dbResult = await client.execute(`select * from tarjay_reviews where product_id=${id} limit 30`);
+  const result = await dbResult.rows.map(val => {
+    return ({
+      _id: val.id,
+      author: val.author,
+      stars: val.stars,
+      body: val.body,
+      createdAt: val.created_at,
+      wouldRecommend: val.would_recommend,
+      title: val.title,
+      comfort: val.comfort,
+      style: val.style,
+      value: val.value,
+      sizing: val.sizing,
+      photos: val.photos,
+      helpfulVotes: val.helpful_votes,
+      productId: val.product_id
+    })
   })
-  .catch(err => console.log('cassandra: following error: ', err));
+  console.log(await result);
+  res.send(await result);
+})
